c1c792a02e7f9226fb4aa765134a62d737d38bff
Henry Eriksson <eriksson.henry@elev.ga.ntig.se>
2021-11-09 14:10:49 +0100

    Initial Program

---
+++ slagsmålspelet/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+Random random = new Random();
+int tantHP;
+int yourHP;
+int randomInt;
+string input;
+
+Console.WriteLine("Börja spelet? ja / nej");
+input = Console.ReadLine();
+
+if(input == "ja"){
+    Console.WriteLine("Okej. Du börjar slåss mot en tant.");
+} else {
+    Console.WriteLine("Öhh, okej? Vi börjar spelet ändå. Du börjar slåss mot en tant.");
+}
