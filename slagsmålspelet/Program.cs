6e83471869b03718559e1ef18cc16954e8ebdf1f
Henry Eriksson <eriksson.henry@elev.ga.ntig.se>
2021-11-23 14:31:29 +0100

    Final Build

--- slagsmålspelet/Program.cs
+++ slagsmålspelet/Program.cs
@@ -1,8 +1,8 @@
 ﻿using System;
 
 Random random = new Random();
-int tantHP;
-int yourHP;
+int tantHP = 100;
+int yourHP = 100;
 int randomInt;
 string input;
 
@@ -11,6 +11,54 @@ input = Console.ReadLine();
 
 if(input == "ja"){
     Console.WriteLine("Okej. Du börjar slåss mot en tant.");
+    Console.ReadKey();
 } else {
     Console.WriteLine("Öhh, okej? Vi börjar spelet ändå. Du börjar slåss mot en tant.");
+    Console.ReadKey();
 }
+
+
+while (tantHP >= 0 && yourHP >= 0){
+    randomInt = random.Next(1,10);
+    if (randomInt >= 6) {
+
+        Console.WriteLine("Tanten ger dig en örfil!");
+
+        randomInt = random.Next(1,10);
+        yourHP -= randomInt;
+
+        Console.WriteLine($"Du har {yourHP}HP || Tant {tantHP}HP");
+        Console.WriteLine();
+        Console.WriteLine("Tryck på nån knapp för att fortsätta...");
+        Console.ReadKey();
+        Console.Clear();
+
+    } else if (randomInt < 6) {
+
+        Console.WriteLine("Du får in ett slag på tanten.");
+
+        randomInt = random.Next(1,10);
+        tantHP -= randomInt;
+    
+        Console.WriteLine($"Du har {yourHP}HP || Tant {tantHP}HP");
+        Console.WriteLine();
+        Console.WriteLine("Tryck på nån knapp för att fortsätta...");
+        Console.ReadKey();
+        Console.Clear();
+    }
+} 
+
+if (tantHP <= 0)
+{
+    Console.WriteLine("Bra jobbat du har mördat en försvarslös tant!");
+    Console.WriteLine("Spelet är nu slut.");
+    Console.ReadKey();
+}
+
+if (yourHP <= 0)
+{
+    Console.WriteLine("Du dog av en tant.");
+    Console.WriteLine("Spelet är nu slut.");
+    Console.ReadKey();
+}
+
